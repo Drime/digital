98e6930ece9cccb9839f51adb2103611a41f0b60
Kevin Bankersen <k.bankersen@gmail.com>
2022-05-20 00:08:47 +0200

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,20 @@
 # set base image (host OS)
 FROM python:3.9.13
 
-# set the port
-EXPOSE 5000
-
 # set the working directory in the container
 WORKDIR /code
+
+# set the port
+EXPOSE 5000
 
 # copy the dependencies file to the working directory
 COPY requirements.txt .
 
-# install dependencies
-RUN pip install -r requirements.txt
-
 # copy the content of the local src directory to the working directory
 COPY src/ .
+
+# install dependencies
+RUN pip install -r requirements.txt
 
 # command to run on container start
 CMD [ "python", "./server.py" ]
--- src/server.py
+++ src/server.py
@@ -2,21 +2,45 @@ from flask import *
 from redis import Redis
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 redis = Redis(host= "redis", port=6379)
 
 @app.route("/", methods=["GET"])
-@app.route("/index", methods=["GET", "POST"])
+@app.route("/index", methods=["GET"])
 def index():
     count = redis.incr("hits")
-    won = count == 100
-    return render_template('index.html',unique=unique ,won=won, count=count)
+    score = redis.get("score")
+    winner = redis.get("winner").decode("utf-8")
+    won = int(count) == int(score)
+    return render_template('index.html', winner=winner, won=won, count=count)
 
-@app.route("/1061334/<int:count>", methods=["GET"])
-def reset(count: int):
+@app.route('/', methods=['POST'])
+@app.route("/index", methods=["POST"])
+def index_post():
+    page_score = redis.get("score")
+    score = int(request.form['score'])
+    if int(page_score) == int(score):
+        winner = request.form['text'].upper()
+        redis.set("winner", winner)
+        #return render_template('index.html', winner=winner, won=True, count=score)
+        return index()
+    else:
+        return "cheater"
+
+@app.route("/220519/reset/<int:count>", methods=["GET"])
+def reset_count(count: int):
     redis.set("hits", count)
-    return "ok"
+    redis.set("winner", "")
+    return redis.get("hits")
+
+@app.route("/220519/score/<int:new_score>", methods=["GET"])
+def set_score(new_score: int):
+    redis.set("score", new_score)
+    redis.set("winner", "")
+    return redis.get("score")
 
 if __name__ == "__main__":
+    redis.set("score", 228)
     app.run(
         host="0.0.0.0",
         port=5000,
